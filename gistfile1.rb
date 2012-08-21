2766ddd3f1d739cb2908b31fbac360d982b1ac17
Dan Rench <dan.rench@tablexi.com>
2012-08-21 10:18:29 -0500

    Show merged status

--- gistfile1.rb
+++ gistfile1.rb
@@ -2,6 +2,10 @@
 
 branches = {}
 
+def is_merged? (upstream, head)
+    IO.popen("git cherry #{upstream} #{head}", 'r').lines.count == 0
+end
+
 IO.popen('git branch -a', 'r').grep(%r{^\s+remotes/origin/\S+$}).map { |b| b.sub(%r{^\s+remotes/}, '').chomp }.each do |branch|
     committer, rel_commit_date, timestamp = `git log -1 --format='%ce%n%cr%n%ct' #{branch}`.chomp.split(/\n/)
     branches[committer] ||= []
@@ -10,7 +14,9 @@ end
 
 branches.each_pair do |k,v|
     print "#{k}:\n"
-    v.sort { |a,b| a[2] <=> b[2] }.map { |b| b[0] + ' (last commit ' + b[1] + ')' }.each do |branch|
+    v.sort { |a,b| a[2] <=> b[2] }.map { |b|
+        b[0] + ' (last commit ' + b[1] + ', ' + (is_merged?('origin/develop', b[0]) ? 'merged' : 'NOT MERGED') + ')'
+    }.each do |branch|
         print "\t#{branch}\n"
     end
     print "\n"
