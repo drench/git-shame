6e1856980577d1d7550ee1e8e26c6e7a1024a37f
Dan Rench <dan.rench@tablexi.com>
2012-10-25 16:51:20 -0500

    branch -r instead of branch -a

--- gistfile1.rb
+++ gistfile1.rb
@@ -13,7 +13,7 @@ def is_merged? (branch)
     $merged_branches.has_key?(branch) && $merged_branches[branch];
 end
 
-IO.popen('git branch -a', 'r').grep(%r{^\s+remotes/origin/\S+$}).map { |b| b.sub(%r{^\s+remotes/}, '').chomp }.each do |branch|
+IO.popen('git branch -r --no-color', 'r').grep(%r{^\s+origin/\S+$}).map { |b| b.chomp.sub(/^\s+/, '') }.each do |branch|
     committer, rel_commit_date, timestamp = `git log -1 --format='%ce%n%cr%n%ct' #{branch}`.chomp.split(/\n/)
     branches[committer] ||= []
     branches[committer] << [ branch, rel_commit_date, timestamp ]
